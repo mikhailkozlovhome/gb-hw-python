1adcd5829a157908e5bc8daa515a9fa4ca10a63f
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-04-21 22:43:12 +0300

    Edit task 24

--- HW-Seminar-4/task24.py
+++ HW-Seminar-4/task24.py
@@ -14,14 +14,22 @@ number_of_elements = int(input("Введите количество кустов
 my_list = [random.randint(1, number_of_elements) for i in range(number_of_elements)]
 print(my_list)
 
+# max_numb_of_berries = 0
+
+# for i in range(len(my_list)):
+#     if i < len(my_list) - 1:
+#         if (my_list[i-1] + my_list[i] + my_list[i+1]) > max_numb_of_berries:
+#             max_numb_of_berries = (my_list[i-1] + my_list[i] + my_list[i+1])
+#     else:
+#         if (my_list[i-1] + my_list[i] + my_list[i+1-len(my_list)]) > max_numb_of_berries:
+#             max_numb_of_berries = (my_list[i-1] + my_list[i] + my_list[i+1])
+        
+# print(f"Максимальное число ягод равно {max_numb_of_berries}")
+
 max_numb_of_berries = 0
 
-for i in range(len(my_list)):
-    if i < len(my_list) - 1:
-        if (my_list[i-1] + my_list[i] + my_list[i+1]) > max_numb_of_berries:
-            max_numb_of_berries = (my_list[i-1] + my_list[i] + my_list[i+1])
-    else:
-        if (my_list[i-1] + my_list[i] + my_list[i+1-len(my_list)]) > max_numb_of_berries:
-            max_numb_of_berries = (my_list[i-1] + my_list[i] + my_list[i+1])
-        
+for i in range(len(my_list)-1):
+    if (my_list[i-1] + my_list[i] + my_list[i+1]) > max_numb_of_berries:
+        max_numb_of_berries = (my_list[i-1] + my_list[i] + my_list[i+1])
+
 print(f"Максимальное число ягод равно {max_numb_of_berries}")
