9cc3d96812a2c91001c8c21eaa02745c20fdea85
Mikhail Kozlov <kozlov_m_v@mail.ru>
2023-06-01 06:02:34 +0300

    Add HW-Seminar-9

---
+++ HW-Seminar-9/view.py
@@ -0,0 +1,8 @@
+import text
+
+def main_menu() -> int:
+    print(text.main_menu)
+    while True:
+        choice = input(text.input_choice)
+        if choice.isdigit() and 0 < choice < 9:
+            return int(choice)
